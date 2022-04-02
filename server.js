bdab9b54ff47238543b7484286e4a51fe47aeced
eugene0928 <umid09tuxtayev@gmail.com>
2022-04-02 14:36:29 +0500

    login post req

--- server.js
+++ server.js
@@ -1,9 +1,24 @@
 const express = require('express')
 const { PORT } = require('./config')
+const fileUpload = require('express-fileupload')
+const cookieParser = require('cookie-parser')
+const path = require('path')
 const url = 'http://192.168.1.8'
 const app = express()
 
+process.db = []
 
+app.use(express.static(path.join(__dirname, 'src', 'public')))
+app.use(cookieParser())
+app.use(fileUpload())
+
+
+// routers
+const loginRouter = require('./src/routes/login.js')
+
+
+
+app.use(loginRouter)
 
 
 app.listen(PORT, () => {
