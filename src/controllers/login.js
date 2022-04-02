f42ef6ae373d48cd7527fcebb618f1ef98bdb52e
eugene0928 <umid09tuxtayev@gmail.com>
2022-04-02 14:36:55 +0500

    login controller

---
+++ src/controllers/login.js
@@ -0,0 +1,59 @@
+const JWT = require('../utils/jwt.js')
+const path = require('path')
+
+const POST = (req, res) => {
+    try {
+        const { username } = req.body
+        const { file } = req.files
+        const db = process.db
+
+        console.log(req.body)
+        console.log(req.files)
+        const user = db.find( el => el.userName == username )
+
+        if(user) {
+            throw new Error('This user already exists!')
+        }
+
+        if(file.size > 5 * 1024 * 1024) {
+            throw new Error('This file is too large!')
+        }
+        const fileName = Date.now() + file.name
+        const newUser = {
+            id: db.length ? db.at(-1).id + 1 : 1,
+            userName: username,
+            file: fileName,
+            turn: db.length == 0,
+            validPlayer: db.length < 2,
+            status: db.length < 2 ? 'Player' : 'Watcher',
+            moves: {}
+        }
+
+        db.push(newUser)
+
+        const filePath = path.join(__dirname, '../', 'uploads', fileName)
+        file.mv(filePath)
+
+        const token = JWT.sign({ id: newUser.id, userName: newUser.userName})
+
+        console.log(db)
+
+        return res
+			.status(200)
+			.cookie('token', token)
+			.json({ 
+				status: 200,
+				message: "You joined the game!" 
+			})
+    } catch (err) {
+        res.status(400)
+            .json({
+                status: 400,
+                message: err.message
+            })
+    }
+}
+
+module.exports = {
+    POST
+}
